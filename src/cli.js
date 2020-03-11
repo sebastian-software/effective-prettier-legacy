ae44fb2474cb8c1bd587927c2bfc5313771167b3
Sebastian Werner <s.werner@sebastian-software.de>
2020-03-11 15:13:59 +0100

    worked on queue-based processing

--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,51 @@
-async function run() {
+import meow from "meow"
+import { CLIEngine } from "eslint"
+import prettier from "prettier"
+import PQueue from "p-queue"
 
+async function run() {
+  const cli = meow(`
+  Usage
+    $ lean-prettier-eslint <input>
+
+  Options
+    --verbose, -v  Increase log level
+
+  Examples
+    $ lean-prettier-eslint filename.js --verbose
+`,
+    {
+      flags: {
+        verbose: {
+          type: "boolean",
+          alias: "v"
+        },
+
+        concurrency: {
+          type: "number",
+          default: 10
+        }
+      }
+    }
+  )
+
+  if (cli.flags.verbose) {
+    console.log("Files: ", cli.input)
+    console.log("Flags: ", cli.flags)
+  }
+
+  const fileTasks = cli.input.map((fileName) => {
+    return async () => {
+      console.log("Processing: ", fileName)
+    }
+  })
+
+  const queue = new PQueue({concurrency: cli.flags.concurrency});
+  queue.on('active', () => {
+    console.log(`Queue Size: ${queue.size} / Pending: ${queue.pending}`);
+  });
+
+  await queue.addAll(fileTasks)
 }
 
 run()
