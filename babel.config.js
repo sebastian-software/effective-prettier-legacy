dba9449b385b0442057b14d06683bbca38f09ed6
Sebastian Werner <s.werner@sebastian-software.de>
2020-03-11 14:55:00 +0100

    initial commit

---
+++ babel.config.js
@@ -0,0 +1,54 @@
+/* eslint-disable import/no-commonjs */
+module.exports = (api) => {
+  const env = api.env()
+  const caller = api.caller((inst) => (inst && inst.name) || "any")
+
+  const isBundler = caller === "rollup-plugin-babel"
+  const isCli = caller === "@babel/node"
+  const isTest = (/\b(test)\b/).exec(env)
+  const modules = (isTest && !isBundler) || isCli ? "commonjs" : false
+  const isUmd = (/\b(umd)\b/).exec(env)
+
+  return {
+    sourceMaps: true,
+    plugins: [
+      [
+        "@babel/proposal-object-rest-spread",
+        {
+          useBuiltIns: true,
+          loose: true
+        }
+      ],
+      isUmd ?
+        null :
+        [
+          "@babel/transform-runtime",
+          {
+            helpers: true,
+            regenerator: false
+          }
+        ]
+    ].filter(Boolean),
+    presets: [
+      [
+        "@babel/env",
+        {
+          exclude: [ "transform-regenerator", "transform-async-to-generator" ],
+          useBuiltIns: "usage",
+          corejs: 3,
+          loose: true,
+          modules
+        }
+      ],
+      [
+        "@babel/typescript",
+        {
+          // We like JSX everywhere. No reason why we have to deal with
+          // legacy type assertion supported in earlier versions.
+          allExtensions: true,
+          isTSX: true
+        }
+      ]
+    ]
+  }
+}
