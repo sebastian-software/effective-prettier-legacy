a6f16ce1683634f2d92ab49b6fcaa546c36bedfa
Sebastian Werner <s.werner@sebastian-software.de>
2020-03-16 10:07:18 +0100

    continued development

--- src/cli.js
+++ src/cli.js
@@ -1,10 +1,12 @@
+import path from "path"
 import meow from "meow"
 import { CLIEngine } from "eslint"
 import prettier from "prettier"
 import PQueue from "p-queue"
 
 async function run() {
-  const cli = meow(`
+  const cli = meow(
+    `
   Usage
     $ lean-prettier-eslint <input>
 
@@ -34,16 +36,52 @@ async function run() {
     console.log("Flags: ", cli.flags)
   }
 
+  const eslintOptions = {
+    cwd: process.cwd(),
+    useEslintrc: true
+  }
+  const eslint = new CLIEngine(eslintOptions)
+
+
   const fileTasks = cli.input.map((fileName) => {
     return async () => {
       console.log("Processing: ", fileName)
+
+      const fileConfig = eslint.getConfigForFile(path.resolve(fileName))
+
+
+      const localEslint = new CLIEngine({
+        plugins: fileConfig.plugins
+      })
+      const rules = localEslint.getRules()
+      const fileRules = fileConfig.rules
+
+      Object.entries(fileRules).forEach(([name, rule]) => {
+        const ruleImpl = rules.get(name)
+        if (ruleImpl) {
+          //console.log(ruleImpl.meta)
+          if (ruleImpl.meta && ruleImpl.meta.fixable) {
+            if (cli.flags.verbose) {
+              //console.log("- Auto fixing: " + name)
+            }
+          } else {
+            // Disable all non-fixable rules
+            fileRules[name] = "off"
+          }
+        } else {
+          console.log("Did not found:", name)
+          // process.exit(1)
+        }
+      })
+
+      //console.log("Config", fileConfig)
     }
   })
 
-  const queue = new PQueue({concurrency: cli.flags.concurrency});
-  queue.on('active', () => {
-    console.log(`Queue Size: ${queue.size} / Pending: ${queue.pending}`);
-  });
+  const queue = new PQueue({ concurrency: cli.flags.concurrency })
+  queue.on("active", () => {
+    console.log(`Queue Size: ${queue.size} / Pending: ${queue.pending}`)
+  })
 
   await queue.addAll(fileTasks)
 }
