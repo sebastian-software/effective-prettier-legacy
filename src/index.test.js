df4400f94b87150ed2018e0b154ff7737b9388f1
Sebastian Werner <s.werner@sebastian-software.de>
2020-03-17 14:44:34 +0100

    worked on test suite

---
+++ src/index.test.js
@@ -0,0 +1,12 @@
+import { promises as fs } from "fs"
+
+import { formatText } from "./"
+
+describe("Auto formatting", () => {
+  test("Autofix 1", async () => {
+    const filePath = "./src/fixtures/autofix1.js"
+    const input = await fs.readFile(filePath, { encoding: "utf-8" })
+    const output = await formatText(input, { filePath })
+    expect(output).toMatchSnapshot()
+  })
+})
