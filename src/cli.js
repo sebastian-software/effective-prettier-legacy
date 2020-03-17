3977a8e429632ee988136ac397ec1dfed44d6edc
Sebastian Werner <s.werner@sebastian-software.de>
2020-03-17 13:27:14 +0100

    Worked on offering fulll API

--- src/cli.js
+++ src/cli.js
@@ -1,16 +1,13 @@
-import { promises as fs } from "fs"
 import path from "path"
 
 import globby from "globby"
 import globParent from "glob-parent"
 import isPathInside from "is-path-inside"
 import meow from "meow"
-import prettier from "prettier"
 import PQueue from "p-queue"
 
-import { getEslintInstance } from "./eslint"
+import { formatFile } from "./index"
 
-const FILE_OPTIONS = { encoding: "utf-8" }
 const CWD = process.cwd()
 
 function verifyInput(paths) {
@@ -27,51 +24,12 @@ function verifyInput(paths) {
   }
 }
 
-function processFileFactory(fileName, cli) {
+function processFileFactory(fileName, options) {
   return async () => {
     console.log(`Processing: ${fileName}...`)
 
     const filePath = path.resolve(fileName)
-    const fixingEslint = getEslintInstance(filePath, cli.flags)
-
-    const fileInput = await fs.readFile(filePath, FILE_OPTIONS)
-
-    const prettierConfig = await prettier.resolveConfig(filePath)
-    const formattedByPrettier = prettier.format(fileInput, {
-      ...prettierConfig,
-      filepath: filePath
-    })
-
-    const report = fixingEslint.executeOnText(formattedByPrettier, filePath)
-
-    if (report.usedDeprecatedRules) {
-      report.usedDeprecatedRules.forEach((deprecationMessage) => {
-        console.warn(`Configuration uses deprecated rule: ${deprecationMessage.ruleId}!`)
-      })
-    }
-
-    const fileResult = report.results[0]
-    if (fileResult) {
-      if (fileResult.messages) {
-        fileResult.messages.forEach((messageEntry) => {
-          console.log(messageEntry)
-        })
-      }
-
-      const fileOutput = fileResult.output
-      if (!fileOutput) {
-        // Nothing was changed!
-        return
-      }
-
-      if (fileInput !== fileOutput) {
-        if (cli.flags.verbose) {
-          console.log(`Writing changes to: ${filePath}...`)
-        }
-
-        await fs.writeFile(filePath, fileOutput, FILE_OPTIONS)
-      }
-    }
+    await formatFile(filePath, options)
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,1 +1,58 @@
-export * from "./eslint"
+import { promises as fs } from "fs"
+
+import prettier from "prettier"
+
+import { getEslintInstance } from "./eslint"
+
+const FILE_OPTIONS = { encoding: "utf-8" }
+
+export async function formatFile(filePath, options) {
+  const fileInput = await fs.readFile(filePath, FILE_OPTIONS)
+  const fileOutput = await formatText(fileInput, { filePath })
+
+  if (fileInput !== fileOutput) {
+    if (options.verbose) {
+      console.log(`Writing changes to: ${filePath}...`)
+    }
+
+    await fs.writeFile(filePath, fileOutput, FILE_OPTIONS)
+  }
+}
+
+export async function formatText(fileInput, options) {
+  const { filePath } = options
+  const fixingEslint = getEslintInstance(filePath, options)
+
+  const prettierConfig = await prettier.resolveConfig(filePath)
+  const formattedByPrettier = prettier.format(fileInput, {
+    ...prettierConfig,
+    filepath: filePath
+  })
+
+  const report = fixingEslint.executeOnText(formattedByPrettier, filePath)
+
+  if (report.usedDeprecatedRules) {
+    report.usedDeprecatedRules.forEach((deprecationMessage) => {
+      console.warn(`Configuration uses deprecated rule: ${deprecationMessage.ruleId}!`)
+    })
+  }
+
+  const fileResult = report.results[0]
+  if (fileResult) {
+    if (fileResult.messages) {
+      fileResult.messages.forEach((messageEntry) => {
+        console.log(messageEntry)
+      })
+    }
+
+    return fileResult.output || fileInput
+  }
+
+  return fileInput
+}
+
+// Compatible to prettier-eslint: https://github.com/prettier/prettier-eslint
+export default function index(options) {
+  const { text, ...cleanOptions } = options
+  return formatText(text, cleanOptions)
+}
