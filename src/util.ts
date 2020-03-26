cdc640a15c5426801f2ad455699e95083efd2427
Sebastian Werner <s.werner@sebastian-software.de>
2020-03-26 14:47:12 +0100

    Auto formatted

--- src/util.ts
+++ src/util.ts
@@ -1,13 +1,14 @@
-import appRootPath from "app-root-path"
 import { spawnSync } from "child_process"
 import { dirname } from "path"
 
-export const ESLINT_ROOT_PATH = (function() {
-  const eslintPkgFile = require.resolve("eslint/package.json")
-  return eslintPkgFile ? dirname(eslintPkgFile) : null
+import appRootPath from "app-root-path"
+
+export const ESLINT_ROOT_PATH = (() => {
+  const eslintPackageFile = require.resolve("eslint/package.json")
+  return eslintPackageFile ? dirname(eslintPackageFile) : null
 })()
 
-export const APP_ROOT_PATH = (function() {
-  const gitResult = spawnSync("git", ["rev-parse", "--show-toplevel"], { encoding: "utf-8" })
+export const APP_ROOT_PATH = (() => {
+  const gitResult = spawnSync("git", [ "rev-parse", "--show-toplevel" ], { encoding: "utf-8" })
   return gitResult && !gitResult.error ? gitResult.stdout : appRootPath.toString()
 })()
